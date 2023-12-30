00f58e0a928b4215bc7a78909415d3b122256ba8
Lucifer472 <JanvI@0427>
2023-12-30 15:25:51 +0530

    let see

--- components/ads/Ad-Category.tsx
+++ components/ads/Ad-Category.tsx
@@ -1,43 +1,56 @@
 "use client";
 import LoadScript from "@/lib/load";
 import { usePathname } from "next/navigation";
-import Script from "next/script";
 import { useEffect } from "react";
 
 const AdTop = () => {
   const pathname = usePathname();
+  // @ts-ignore
+  window.googletag = window.googletag || { cmd: [] };
 
   useEffect(() => {
     const loadAds = async () => {
       LoadScript(() => {
         // @ts-ignore
-        if (googletag) {
+        googletag.cmd.push(function () {
           // @ts-ignore
-          googletag.cmd.push(function () {
+          googletag
+            .defineSlot(
+              "/22989534981/DG_7_336X280",
+              [336, 280],
+              "div-gpt-ad-1700655338779-0"
+            )
             // @ts-ignore
-            googletag.display("div-gpt-ad-1700655338779-0");
-            console.log("Pushing");
-          });
-        } else {
-          console.log("Not working");
-        }
+            .addService(googletag.pubads());
+          // @ts-ignore
+          googletag.pubads().enableSingleRequest();
+          // @ts-ignore
+          googletag.enableServices();
+          // @ts-ignore
+          googletag.display("div-gpt-ad-1700655338779-0");
+        });
       });
     };
     loadAds().then(() => {
       console.log("ITS WORKING");
     });
+
+    return () => {
+      // Clean up the ad slot when the component unmounts or pathname changes
+      // @ts-ignore
+      if (googletag) {
+        // @ts-ignore
+        googletag.cmd.push(function () {
+          console.log("SEE");
+          // @ts-ignore
+          googletag.destroySlots();
+        });
+      }
+    };
   }, [pathname]);
 
   return (
     <>
-      <Script id="Top-Ad" strategy="afterInteractive">
-        {`window.googletag = window.googletag || {cmd: []};
-            googletag.cmd.push(function() {
-                googletag.defineSlot('/22989534981/DG_7_336X280', [336, 280], 'div-gpt-ad-1700655338779-0').addService(googletag.pubads());
-                googletag.pubads().enableSingleRequest();
-                googletag.enableServices();
-            });`}
-      </Script>
       <div
         id="div-gpt-ad-1700655338779-0"
         style={{ minWidth: "336px", minHeight: "280px" }}
