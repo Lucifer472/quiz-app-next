2b60f7270afcbe6a65ef42c124b2f3b529954197
Lucifer472 <JanvI@0427>
2023-12-30 11:19:49 +0530

    ad new fix

--- components/ads/AdTop.tsx
+++ components/ads/AdTop.tsx
@@ -8,9 +8,7 @@ const AdTop = () => {
         style={{ minWidth: "336px", minHeight: "280px;" }}
       ></div>
       <Script strategy="afterInteractive" id="Top-Ad">
-        {`googletag.cmd.push(function(){
-            googletag.display('div-gpt-ad-1700655338779-0');
-        })`}
+        {`googletag.cmd.push(function() { googletag.display('div-gpt-ad-1700655338779-0'); });`}
       </Script>
     </>
   );
