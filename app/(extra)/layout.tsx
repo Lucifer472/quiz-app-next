aa77a83cac461b63badcee520a1f40da01bb7f21
Lucifer472 <JanvI@0427>
2024-01-16 16:45:32 +0530

    meta change

---
+++ app/(extra)/layout.tsx
@@ -0,0 +1,14 @@
+import { facts } from "@/constant";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "DG Quiz",
+  description: facts,
+  keywords: "Education, Online Quiz,  Play Quiz, Win Coin, GK, Question Answer",
+};
+
+const StartLayout = ({ children }: { children: React.ReactNode }) => {
+  return { children };
+};
+
+export default StartLayout;
--- constant/index.ts
+++ constant/index.ts
@@ -23,3 +23,16 @@ export const icons: { [key: string]: string } = {
   "Bollywood Quiz": "trivia-min.png",
   "Hollywood Quiz": "vocab.png",
 };
+
+export const facts = `
+The insurance industry is one of the largest industries in the United
+          States, with over $1.5 trillion in annual premiums.The word
+          &quot;insurance&quot; comes from the French word &quot;assurer&quot;,
+          which means &quot;to make sure&quot;. The first insurance company in
+          the United States was founded in Charleston, South Carolina, in
+          1735.The insurance industry employs over 2 million people in the
+          United States. The average American household spends about $1,500 per
+          year on insurance premiums. The most expensive type of insurance in
+          the United States is long-term care insurance, which can cost upwards
+          of $5,000 per month.
+          `;
