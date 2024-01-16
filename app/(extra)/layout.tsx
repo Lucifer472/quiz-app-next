9efb6d3dd008275add78f4843b99e95163d23cc9
Lucifer472 <JanvI@0427>
2024-01-16 16:46:56 +0530

    children fix

--- app/(extra)/layout.tsx
+++ app/(extra)/layout.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
 };
 
 const StartLayout = ({ children }: { children: React.ReactNode }) => {
-  return { children };
+  return children;
 };
 
 export default StartLayout;
